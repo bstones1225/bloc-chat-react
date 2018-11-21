a8d642f8cc0942471664ccadbd36cbe61314a3f9
Brian Stones <bstones1225@hotmail.com>
2018-11-21 02:56:14 -0600

    Message component update

--- src/components/Message.js
+++ src/components/Message.js
@@ -1,27 +1,30 @@
 import React, { Component } from 'react'
 import './Message.css';
+import classNames from "classnames"
 import PropTypes from 'prop-types'
 
 class Message extends Component {
   static propTypes = {
-    username: PropTypes.string,
-    roomId: PropTypes.string,
     content: PropTypes.string.isRequired,
+    me:PropTypes.bool,
+
 
   }
 
   render() {
+    const classes = classNames('Message', {
+
+      me: this.props.me
+    })
+
     return (
-      <div className="Message">
+    <div className={classes}>
       {this.props.username && (
         <span className="username">{this.props.username}:</span>
       )}
       {this.props.content}
       {this.props.roomId}
-
-
-
-      </div>
+    </div>
     )
   }
 }
